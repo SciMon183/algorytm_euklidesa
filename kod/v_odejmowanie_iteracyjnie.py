4e80cf5d47610916ab727f4b5da86d532b81e02e
Szymon <125824420+SciMon183@users.noreply.github.com>
2023-08-19 20:57:14 +0200

    Update v_odejmowanie_iteracyjnie.py

---
+++ kod/v_odejmowanie_iteracyjnie.py
@@ -0,0 +1,13 @@
+# NWD z odejmowaniem iteracyjnym 
+def NWD(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+a = int(input('Podaj liczbę a: '))
+b = int(input('Podaj liczbę b: '))
+
+print('NWD wynosi: ', NWD(a, b))
